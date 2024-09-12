32833f41b58120bb7f0cd45ca90a59a745554a11
natninac <natninac@gmail.com>
2024-09-12 15:59:39 -0400

    Programmed a canvas!

--- topics/function-calls/js/script.js
+++ topics/function-calls/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+createCanvas(640,480)
 }
 
 
@@ -20,5 +20,8 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+background(255,200,100)
+rect(80,50,240,350)
+rect(380,50,240,200)
+rect(80,420,500,40)
 }
