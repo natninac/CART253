e3a65be70cfaca98a499511469c5cbdf7099f6d6
natninac <natninac@gmail.com>
2024-09-29 16:08:45 -0400

    Fixed the template of the folder

--- topics/mouse-events/js/script.js
+++ topics/mouse-events/js/script.js
@@ -1,66 +1,30 @@
 /**
- * Mouse Events
- * Pippin Barr
+ * Art Jam
+ * Nat Nina
  * 
- * A chance to experiment with mouse events in a simple setting.
-*/
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
 
 "use strict";
 
-// Our ball
-const ball = {
-    // Position
-    x: 0,
-    y: 200,
-    // Size
-    size: 50,
-    // Velocity so it can move
-    velocity: {
-        x: 0,
-        y: 0
-    },
-    // Speed when it moves
-    speed: 5
-}
-//Ball moves when mouse pressed
-function mousePressed() {
-    ball.velocity.x = ball.speed;
-}
-/**
- * Stops the ball moving
- */
-function mouseReleased() {
-    ball.velocity.x = 0;
-}
 
 /**
- * Grows the ball when the mouse wheel is rolled
- */
-function mouseWheel() {
-    ball.size += 0.5;
-}
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
 
-/**
- * Creates the canvas
- */
+//setup function
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(500, 500);
 }
 
-/**
- * Moves the ball and draws it
- */
+//draw function
 function draw() {
-    background(0);
-
-    // Move the ball
-    ball.x += ball.velocity.x
-    ball.y += ball.velocity.y;
-
-    // Draw the ball
-    push();
-    ellipse(ball.x, ball.y, ball.size);
-    pop();
-
-
+  //change the entire canvas to black if mouseX is at the far right end
+  if (mouseX >= 480) {
+    background(0); // black background
+    return; // exit draw to prevent drawing other shapes
+  } else {
+    background(135, 206, 235);
+  }
 }
